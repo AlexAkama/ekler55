1515ccee46db9fae66e12ccdef63b4e3c40649f6
Alex Akama <gremcox@bk.ru>
2021-01-20 12:03:29 +0500

    Added scroll from menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $(function() {
-
+    // Плавный скролл
+    $('.nav__link').on('click', function(event) {
+        event.preventDefault();
+        var id = $(this).attr('href');
+        var element = $(id).offset().top;
+        $('body,html').animate({
+            scrollTop: element
+        }, 500);
+    });
 });
